abc17c812c4b7c900b7573adbd870a9e056d1f46
nailtonroberiosantosxavier-droid <nailtonroberiosantosxavier@gmail.com>
2026-04-12 16:16:11 -0300

    P31C workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.p31c"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
